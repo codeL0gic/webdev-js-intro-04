e3cbaf6932b0336911e13472e5cc024f64860b0d
codeL0gic <shannon.neumann@yum.com>
2025-01-12 18:31:13 -0500

    main

--- js/assignment.js
+++ js/assignment.js
@@ -1,30 +1,35 @@
 "use strict";
 
 // Create your references to the html elements here
-const ageInputEl = document.getElementById("age-input")
+const ageInputEl = document.getElementById("age-input");
 const submissionBtn = document.getElementById("submission-btn");
-
+const getResponse = document.getElementById("response");
 // create a mutable variable called age and do not assign it a value.
+let age <= 18;
 
 
 function checkAgeAndRespond() {
-    age = parseInt(ageInputEl.value);
-    
-    // Write your code below
+  age = parseInt(ageInputEl.value);
 
+  // Write your code below
+  if (age => 21) {
+    console.log("You can vote, and purchase alcohol.");
+  } else if (ageInputEl.value >= 18 && ((ageInputEl.value) => 21)) {
+    console.log("You can vote but you cannot purchase alcohol.");
+  } else {
+    console.log("You cannot vote and you cannot purchase alcohol.");
+  }
 }
 
-
 submissionBtn.addEventListener("click", function () {
-    checkAgeAndRespond();
+  checkAgeAndRespond();
 });
 
-
 function validateInput(event) {
-    const invalidCharacters = ["e", "E", "-", "+"];
-    if (invalidCharacters.includes(event.key)) {
-        event.preventDefault();
-    }
+  const invalidCharacters = ["e", "E", "-", "+"];
+  if (invalidCharacters.includes(event.key)) {
+    event.preventDefault();
+  }
 }
 
 ageInputEl.addEventListener("keydown", validateInput);
--- js/index.js
+++ js/index.js
@@ -1,3 +1,14 @@
 // Prevent us from attempting to use variables
 // that are not declared
-"use strict"
+"use strict";
+/*
+let healthPoints = 75;
+
+if (healthPoints >= 60) {
+  console.log("Hero is healthy.");
+} else if (healthPoints < 10 && healthPoints >= 5) {
+  console.log("Hero is slightly damaged.");
+} else {
+  console.log("Hero is close to death.");
+}
+*/
